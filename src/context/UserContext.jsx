31056cd590d9a07fe83fa0877b70ba53b4112a4d
Your Name <youremail@example.com>
2025-04-08 15:19:52 +0530

    fix: corrected UserContext file and import

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,25 +1,21 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 
-export const UserContextData = createContext()
+export const UserContextData = createContext();
 
-const UserContext = ({children}) => {
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: '',
+    fullName: {
+      firstName: '',
+      lastName: '',
+    },
+  });
 
-    const [user, setuser] = useState({
-        email: "",
-        fullName:{
-            firstName: "",
-            lastName: "",
-        },
-         
-    })
   return (
-    <div>
-        
-        <UserContextData.Provider value={[user, setuser] }>
-            {children}
-        </UserContextData.Provider>
-    </div>
-  )
-}
+    <UserContextData.Provider value={[user, setUser]}>
+      {children}
+    </UserContextData.Provider>
+  );
+};
 
-export default UserContext
+export default UserContext;
